ac4dab6b514d07f70d78fb609585dcb750515753
茳尒卂 <90538662+jex12527@users.noreply.github.com>
2022-07-17 14:56:13 +0800

    Update GitTest.java
    虞书欣

--- src/main/java/com/jex/git/GitTest.java
+++ src/main/java/com/jex/git/GitTest.java
@@ -3,10 +3,6 @@ package com.jex.git;
 public class GitTest {
 
     public static void main(String[] args){
-        System.out.println("hello jex!");
-        System.out.println("第一次修改！");
-        System.out.println("第二次修改！");
-        System.out.println("jex分支修改！");
-        System.out.println("jex最后的修改！");
+        System.out.println("我喜欢虞书欣！");
     }
 }
